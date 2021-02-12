5e0020b743201c21714062b6d1d3c1fd3842c14c
IsmaelPereira <ismaelfgpereira@gmail.com>
2021-02-12 19:06:13 -0300

    handler.go refactoring

--- handler/handler.go
+++ handler/handler.go
@@ -7,7 +7,7 @@ import (
 
 //VerifyAndExecuteCommand is a function to pick the right command and execute the respective function
 func VerifyAndExecuteCommand(cmd string, bot *tgbotapi.BotAPI, update *tgbotapi.Update) error {
-	var commands map[string]func(*tgbotapi.BotAPI, *tgbotapi.Update) error = map[string]func(*tgbotapi.BotAPI, *tgbotapi.Update) error{
+	commands := map[string]func(*tgbotapi.BotAPI, *tgbotapi.Update) error{
 		"help":    controllers.HelpHandlerUpdate,
 		"admiral": controllers.AdmiralHandleUpdate,
 		"anime":   controllers.AnimeHandleUpdate,
@@ -17,14 +17,8 @@ func VerifyAndExecuteCommand(cmd string, bot *tgbotapi.BotAPI, update *tgbotapi.
 	}
 	if f, ok := commands[cmd]; ok {
 		err := f(bot, update)
-		if err != nil {
-			return err
-		}
-		return nil
-	}
-	err := controllers.NotFoundHandlerUpdate(bot, update)
-	if err != nil {
 		return err
 	}
-	return nil
+	err := controllers.NotFoundHandlerUpdate(bot, update)
+	return err
 }

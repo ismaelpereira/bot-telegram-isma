8b8b216fd789fda770c9a263c4b2bae4bed61b81
IsmaelPereira <ismaelfgpereira@gmail.com>
2021-02-12 17:49:40 -0300

    removing all go.mod comment problems

--- handler/handler.go
+++ handler/handler.go
@@ -7,11 +7,7 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-//var Commands []string = []string{"help", "admiral", "anime", "manga", "money", "movie"}
-// var Commands map[string]interface{} = map[string]interface{}{
-// 	"help": controllers.HelpHandlerUpdate(bot, update),
-// }
-
+//VerifyAndExecuteCommand is a function to pick the right command and execute the respective function
 func VerifyAndExecuteCommand(cmd string, bot *tgbotapi.BotAPI, update *tgbotapi.Update) error {
 	var commands map[string]func(*tgbotapi.BotAPI, *tgbotapi.Update) error = map[string]func(*tgbotapi.BotAPI, *tgbotapi.Update) error{
 		"help":    controllers.HelpHandlerUpdate,
@@ -27,6 +23,12 @@ func VerifyAndExecuteCommand(cmd string, bot *tgbotapi.BotAPI, update *tgbotapi.
 			log.Println(err)
 			return err
 		}
+		return nil
+	}
+	err := controllers.NotFoundHandlerUpdate(bot, update)
+	if err != nil {
+		log.Println(err)
+		return err
 	}
 	return nil
 }
